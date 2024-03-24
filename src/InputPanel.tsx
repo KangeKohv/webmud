a770a23ceeb743556df2333b0e270519d785bdf2
KangeKohv <23702881+KangeKohv@users.noreply.github.com>
2024-03-24 23:38:52 +0200

    Remove redundant logging of inputs

--- src/InputPanel.tsx
+++ src/InputPanel.tsx
@@ -10,14 +10,12 @@ export const InputPanel = () => {
         if(e.key === "Enter") {
             e.preventDefault();
             send(text);
-            console.log("Sent ", text);
             setText("");
         }
     }
 
     const onChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
         setText(e.target.value);
-        console.log("onChange, set", e.target.value);
     }
 
     return <textarea className="h-full w-full border-2" value={text} onKeyDown={onInput} onChange={onChange}/>
